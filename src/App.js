923bbf6fab7cd5d5633c4eda67a8262c1d987946
CintaChan86 <80836951+CintaChan86@users.noreply.github.com>
2021-07-26 23:08:56 +0200

    Added Form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 
 import './App.css';
+import Dictionary from './Dictionary';
+import logo from './logo.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Hello Dictionary
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small><a href="https://github.com/CintaChan86/dictionary"> Coded </a> by Yacinta Hostiningtyas</small>
+        </footer>
+      </div>
     </div>
   );
 }
